eb81a7d7932d8ce6644a5f24ead68be441325cb0
Lucas Santos Ribeiro <lucas.cooper25@gmail.com>
2025-03-06 14:02:29 -0300

    Questão 4

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import sumResult from './scripts/sumResult.js';
 import Fibonacci from './scripts/Fibonacci.js';
 import fetchData from './scripts/biling.js';
+import stateShare from './scripts/stateShare.js';
 
 sumResult();
 Fibonacci();
 fetchData();
+stateShare();
