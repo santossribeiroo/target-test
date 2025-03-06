75fd6a4db8a72c6bb303626d94aaeac2ec0a2ab4
Lucas Santos Ribeiro <lucas.cooper25@gmail.com>
2025-03-06 14:08:59 -0300

    Questão 5

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import sumResult from './scripts/sumResult.js';
 import Fibonacci from './scripts/Fibonacci.js';
 import fetchData from './scripts/biling.js';
 import stateShare from './scripts/stateShare.js';
+import reverseWord from './scripts/word.js';
 
 sumResult();
 Fibonacci();
 fetchData();
 stateShare();
+reverseWord();
