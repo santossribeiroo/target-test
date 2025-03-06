3bf4de9464c56b2c78881fe23074fc9342d1b37d
Lucas Santos Ribeiro <lucas.cooper25@gmail.com>
2025-03-06 13:32:52 -0300

    Questão 3 - Final

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import sumResult from './scripts/sumResult.js';
 import Fibonacci from './scripts/Fibonacci.js';
-import fetch from './scripts/biling.js';
+import fetchData from './scripts/biling.js';
 
 sumResult();
 Fibonacci();
-fetch();
+fetchData();
